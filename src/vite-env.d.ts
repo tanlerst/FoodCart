e0aed05264ac1152539779205f7ed2089c4ac10c
tanlerst <45229323+tanlerst@users.noreply.github.com>
2026-05-28 21:35:46 +0800

    Delete src directory

--- src/vite-env.d.ts
+++
@@ -1,9 +0,0 @@
-interface ImportMetaEnv {
-  readonly VITE_SUPABASE_URL: string;
-  readonly VITE_SUPABASE_PUBLISHABLE_KEY: string;
-  // add any other custom variables here
-}
-
-interface ImportMeta {
-  readonly env: ImportMetaEnv;
-}
