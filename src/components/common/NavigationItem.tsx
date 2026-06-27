3e430702b8a56d476e5f6a7eae0cf16221d0781c
Xy <67408718+xiiyann@users.noreply.github.com>
2026-06-27 14:55:56 +0800

    Separated navigation item from navigation bar

---
+++ src/components/common/NavigationItem.tsx
@@ -0,0 +1,24 @@
+/* Navigation Item to be used by navigation bar*/
+type NavigationItemProps = {
+  icon: string;
+  label: string;
+  onClick?: () => void;
+};
+
+export default function NavigationItem({ icon, label, onClick }: NavigationItemProps) {
+    return (
+        <button 
+            className="nav-item flex flex-col items-center gap-1" 
+            onClick={onClick}
+            >
+            
+            <span className="text-xl">
+                {icon}
+            </span>
+
+            <span className="text-xs">
+                {label}
+            </span>
+        </button>
+    );
+}
