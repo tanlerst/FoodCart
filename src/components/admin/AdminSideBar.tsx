c7bbe93bc50e8503cf2afeac2701232d8f6ff2ec
Xy <67408718+xiiyann@users.noreply.github.com>
2026-06-27 23:35:07 +0800

    Updated admin side bar so that the coresponding bar is highlighted when clicked

--- src/components/admin/AdminSideBar.tsx
+++ src/components/admin/AdminSideBar.tsx
@@ -1,7 +1,7 @@
 /* Admin portal navigation bar */
 
 import LogoutButton from "../../components/admin/LogoutButton";
-import { useNavigate } from "react-router";
+import { NavLink, useNavigate } from "react-router";
 
 type SidebarItem = {
   label: string;
@@ -27,32 +27,39 @@ const sidebarItems: SidebarItem[] = [
   },
 ];
 
+const BASE_BAR_FORMAT = "flex items-center gap-4 rounded-xl px-4 py-3 text-sm font-medium transition";
 const ACTIVE_BAR_FORMAT = "bg-orange-50 text-orange-600";
 const INACTIVE_BAR_FORMAT = "text-gray-600 hover:bg-orange-50 hover:text-orange-600";
 
+function getSidebarItemClass(isActive: boolean) {
+    const baseBarFormat = BASE_BAR_FORMAT;
+    const barFormat = isActive ? ACTIVE_BAR_FORMAT : INACTIVE_BAR_FORMAT;
+    
+    return `${baseBarFormat} ${barFormat}`
+}
+
 export default function AdminSideBar() {
-  const navigate = useNavigate();
-  return (
+    const navigate = useNavigate();
+
+    return (
     <aside className="flex min-h-screen w-64 flex-col border-r border-orange-100 bg-white px-4 py-6">
-      {/* Side Bar */}
-      <nav className="flex flex-1 flex-col gap-2">
-        {sidebarItems.map((item, index) => (
-          <button
-            key={item.label}
-            className={`flex items-center gap-4 rounded-xl px-4 py-3 text-sm font-medium transition ${
-              index === 0 ? ACTIVE_BAR_FORMAT : INACTIVE_BAR_FORMAT
-            }`}
-            onClick={() => navigate(item.route)}
-          >
-            <span className="text-xl">{item.icon}</span>
+        {/* Side Bar */}
+        <nav className="flex flex-1 flex-col gap-2">
+            {sidebarItems.map((item) => (
+                <NavLink
+                    key={item.label}
+                    to={item.route} // navigation
+                    className={({ isActive }) => getSidebarItemClass(isActive)}
+                    >
+                    <span className="text-xl">{item.icon}</span>
 
-            <span>{item.label}</span>
-          </button>
-        ))}
-      </nav>
+                    <span>{item.label}</span>
+                </NavLink>
+            ))}
+        </nav>
 
-      {/* Logout */}
-      <LogoutButton />
+        {/* Logout */}
+        <LogoutButton />
     </aside>
-  );
+    );
 }
