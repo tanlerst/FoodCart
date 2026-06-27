7d6d88857aca4176c6afc20b6b43792d87db251d
Xy <67408718+xiiyann@users.noreply.github.com>
2026-06-27 14:14:11 +0800

    Updated admin order page

--- src/pages/admin/AdminOrderPage.tsx
+++ src/pages/admin/AdminOrderPage.tsx
@@ -3,18 +3,28 @@
 import { useState } from "react";
 import StatusFilterBar from "../../components/admin/order/StatusFilterBar";
 import OrderTable from "../../components/admin/order/OrderTable";
+import AdminSideBar from "../../components/admin/AdminSideBar"
 
 export default function AdminOrdersPage() {
   const [status, setStatus] = useState("all");
 
   return (
-    <div className="min-h-screen bg-orange-50 px-8 py-8">
-      <StatusFilterBar status={status} onChange={setStatus} />
+    <div className="flex min-h-screen bg-gray-50">
+      <AdminSideBar/>
+    
+      <main className="flex-1 bg-orange-50 px-8 py-8">
+        <StatusFilterBar 
+          status={status} 
+          onChange={setStatus} 
+        />
 
-      <div className="p-4">
-        <h1 className="text-2xl font-bold mb-4">Admin Order Page</h1>
-        <OrderTable />
-      </div>
+        <div className="p-4">
+          <h1 className="text-2xl font-bold mb-4">
+            Admin Order Page
+          </h1>
+          <OrderTable />
+        </div>
+      </main>
     </div>
   );
 }
