12db8f60d0a89ad9308c1810e22e78e6f1d16700
Xy <67408718+xiiyann@users.noreply.github.com>
2026-06-27 22:35:37 +0800

    Updated nav item and menu page navigation bar

--- src/components/common/NavigationBar.tsx
+++ src/components/common/NavigationBar.tsx
@@ -7,30 +7,14 @@
 
 */
 
-/* currently hardcoded to categories being selected */
-import home from "../../assets/menu/navigation_bar/home_grey.png";
-import categories from "../../assets/menu/navigation_bar/categories_orange.png";
-import cart from "../../assets/menu/navigation_bar/cart_grey.png";
-import orders from "../../assets/menu/navigation_bar/order_grey.png";
-import profile from "../../assets/menu/navigation_bar/user_grey.png";
 import NavigationItem from "./NavigationItem";
+import { IoHomeOutline } from "react-icons/io5";
+import { BiSolidCategoryAlt } from "react-icons/bi";
+import { FaShoppingCart } from "react-icons/fa";
+import { CgNotes } from "react-icons/cg";
+import { IoPerson } from "react-icons/io5";
 import { useNavigate } from "react-router-dom";
-/*
-type NavigationItemProps = {
-  imgSrc: string;
-  label: string;
-  onClick?: () => void;
-};
 
-function NavigationItem({ imgSrc, label, onClick }: NavigationItemProps) {
-  return (
-    <button className="nav-item flex flex-col items-center gap-1" onClick={onClick}>
-      <img src={imgSrc} alt={label} className="w-6 h-6" />
-      <span className="text-xs">{label}</span>
-    </button>
-  );
-}
-*/
 export default function NavigationBar() {
   const navigate = useNavigate();
 
@@ -38,19 +22,19 @@ export default function NavigationBar() {
     <div className="navigation-bar text-gray p-4 mt-6 border-t">
       <ul className="flex justify-around">
         <li>
-          <NavigationItem icon={home} label="Home" onClick={() => navigate("/menu")} />
+          <NavigationItem icon={IoHomeOutline} label="Home" onClick={() => navigate("/menu")} />
         </li>
         <li>
-          <NavigationItem icon={categories} label="Categories" onClick={() => navigate("/menu")} />
+          <NavigationItem icon={BiSolidCategoryAlt} label="Categories" onClick={() => navigate("/menu")} />
         </li>
         <li>
-          <NavigationItem icon={cart} label="Cart" onClick={() => navigate("/cart")} />
+          <NavigationItem icon={FaShoppingCart} label="Cart" onClick={() => navigate("/cart")} />
         </li>
         <li>
-          <NavigationItem icon={orders} label="Orders" onClick={() => navigate("/orders")} />
+          <NavigationItem icon={CgNotes} label="Orders" onClick={() => navigate("/orders")} />
         </li>
         <li>
-          <NavigationItem icon={profile} label="Profile" onClick={() => navigate("/profile")} />
+          <NavigationItem icon={IoPerson} label="Profile" onClick={() => navigate("/profile")} />
         </li>
       </ul>
     </div>
--- src/components/common/NavigationItem.tsx
+++ src/components/common/NavigationItem.tsx
@@ -1,16 +1,23 @@
 /* Navigation Item to be used by navigation bar*/
+import type { IconType } from "react-icons";
+
 type NavigationItemProps = {
-  icon: string;
+  icon: string | IconType;
   label: string;
   onClick?: () => void;
 };
 
 export default function NavigationItem({ icon, label, onClick }: NavigationItemProps) {
-  return (
-    <button className="nav-item flex flex-col items-center gap-1" onClick={onClick}>
-      <span className="text-xl">{icon}</span>
+    const isStringIcon = typeof icon === "string";
+    const Icon = icon;
 
-      <span className="text-xs">{label}</span>
-    </button>
-  );
+    return (
+        <button className="nav-item flex flex-col items-center gap-1" onClick={onClick}>
+            <span className="text-xl">
+                {isStringIcon ? icon : <Icon />}
+            </span>
+
+            <span className="text-xs">{label}</span>
+        </button>
+    );
 }
