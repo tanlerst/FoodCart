9b98aeb15157cf5478ef0239dbe20d01c62e9664
Xy <67408718+xiiyann@users.noreply.github.com>
2026-07-08 19:57:55 +0800

    Added surprise me button for surprise option page

---
+++ src/components/surprise/surpriseOption/SurpriseMeButton.tsx
@@ -0,0 +1,26 @@
+/**
+ *  @file SurpriseMeButton.tsx
+ *  @author Xi Yan 
+ *  @version 1.0.0
+ *  @description This file is the surprise me button component for the surprise option page. 
+ *                It displays a button that triggers the surprise action.
+ */
+
+type SurpriseMeButtonProps = {
+    onSurprise: () => void;
+};
+
+export default function SurpriseMeButton({ onSurprise }: SurpriseMeButtonProps) {
+  return (
+    <button
+      type="button"
+      onClick={onSurprise}
+      className="w-100 rounded-xl bg-orange-400 px-6 py-3 font-bold text-white"
+    >
+      Surprise Me
+    </button>
+  );
+}
+
+
+
