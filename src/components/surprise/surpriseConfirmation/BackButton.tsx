ddbfd8924461a830557a6daa46ab33b9a2f23c2c
Xy <67408718+xiiyann@users.noreply.github.com>
2026-07-15 03:14:54 +0800

    Added back button and place order button to surprise confirmation page

---
+++ src/components/surprise/surpriseConfirmation/BackButton.tsx
@@ -0,0 +1,26 @@
+/**
+ *  @file BackButton.tsx
+ *  @author Xi Yan 
+ *  @version 1.0.0
+ *  @description This file is the back button component for the surprise confirmation page. 
+ *                
+ */
+
+type BackButtonProps = {
+    onClick: () => void;
+};
+
+export default function BackButton({ onClick }: BackButtonProps) {
+    return (
+        <button
+            type="button"
+            onClick={onClick}
+            className="min-h-14 rounded-xl border-2 border-orange-500 px-6 py-3 font-bold text-orange-500"
+        >
+            Back
+        </button>
+    );
+}
+
+
+
--- src/pages/surprise/SurpriseConfirmationPage.tsx
+++ src/pages/surprise/SurpriseConfirmationPage.tsx
@@ -5,3 +5,26 @@
  *  @description This file is the surprise confirmation page. 
  *                
  */
+
+import BackButton from "../../components/surprise/surpriseConfirmation/BackButton"
+import PlaceOrderButton from "../../components/surprise/surpriseConfirmation/PlaceOrderButton";
+
+
+export default function SurpriseConfrmationPage() {
+
+    return (
+
+        <main className="min-h-screen bg-orange-50 p-4">
+            <div className="relative mx-auto min-h-screen max-w-md rounded-2xl p-10 shadow-md">
+            
+                {/* Buttons */}
+                <div className="mt-auto grid grid-cols-[0.9fr_1.1fr] gap-4 pt-8">
+                    <BackButton onClick={() => {}} />
+
+                    <PlaceOrderButton onClick={() => {}} />
+                </div>
+
+            </div>
+        </main>
+    );
+}
