d05ac8498ace2eb3b590308c3513e693b21a8280
Xy <67408718+xiiyann@users.noreply.github.com>
2026-07-15 02:02:55 +0800

    Added Mark All Item As Served Button, Mark As Paid Button, Mark Order Complete Button, Start Preparing Button for Order Details page

---
+++ src/components/admin/order/MarkAllAsServedButton.tsx
@@ -0,0 +1,29 @@
+/**
+ *  @file MarkAllAsServedButton.tsx
+ *  @author Xi Yan 
+ *  @version 1.0.0
+ *  @description This file is the mark all as served button component for the order details page.
+ *               This is used to mark all items' statuses as served.         
+ */
+
+type MarkAllAsServedButtonProps = {
+    onClick: () => void;
+    disabled?: boolean;
+}
+
+export default function MarkAllAsServedButton({
+    onClick,
+    disabled,
+}:MarkAllAsServedButtonProps){
+    return (
+
+        <button
+            type="button"
+            onClick={onClick}
+            disabled={disabled}
+            className="rounded-xl bg-orange-500 px-6 py-3 font-semibold text-white transition hover:bg-orange-600 active:scale-[0.98]"
+            >
+            Mark All as Served
+        </button>
+    );
+}
