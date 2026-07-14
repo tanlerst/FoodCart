4fe548f3e278415ca56c3dd5b83a41c076606c28
Xy <67408718+xiiyann@users.noreply.github.com>
2026-07-15 03:18:30 +0800

    Created files for surprise confirmation page components

---
+++ src/components/surprise/surpriseConfirmation/ConfirmSurpriseHeader.tsx
@@ -0,0 +1,8 @@
+/**
+ *  @file ConfirmSurpriseHeader.tsx
+ *  @author Xi Yan 
+ *  @version 1.0.0
+ *  @description This file is the Confirm Surprise Header component for the surprise confirmation page. 
+ *                
+ */
+
---
+++ src/components/surprise/surpriseConfirmation/ConfirmSurpriseTotal.tsx
@@ -0,0 +1,8 @@
+/**
+ *  @file ConfirmSurpriseTotal.tsx
+ *  @author Xi Yan 
+ *  @version 1.0.0
+ *  @description This file is the Confirm Surprise Total component for the surprise confirmation page that consists of the total price. 
+ *                
+ */
+
