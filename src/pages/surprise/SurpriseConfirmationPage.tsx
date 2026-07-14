98155464d689f2cd1fcccc0c8ed1984e2e5bac32
Xy <67408718+xiiyann@users.noreply.github.com>
2026-07-14 15:54:55 +0800

    Added surprise confirmation page and order placed page files

---
+++ src/pages/surprise/SurpriseConfirmationPage.tsx
@@ -0,0 +1,7 @@
+/**
+ *  @file SurpriseConfrmationPage.tsx
+ *  @author Xi Yan 
+ *  @version 1.0.0
+ *  @description This file is the surprise confirmation page. 
+ *                
+ */
---
+++ src/pages/surprise/SurpriseOrderPlacedPage.tsx
@@ -0,0 +1,6 @@
+/**
+ *  @file CategoryCard.tsx
+ *  @author Xi Yan 
+ *  @version 1.0.0
+ *  @description This file is the surprise order placed page.           
+ */
