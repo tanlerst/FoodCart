4c9096261a7a1c7b758e5c109e5bcdb779037a45
Xy <67408718+xiiyann@users.noreply.github.com>
2026-07-18 03:15:34 +0800

    Updated admin dashboard page to implement admin layout

--- src/pages/admin/AdminDashboardPage.tsx
+++ src/pages/admin/AdminDashboardPage.tsx
@@ -1,21 +1,16 @@
 /* Admin Dashboard Page showing top selling food items and total sales */
 
-import AdminSideBar from "../../components/admin/AdminSideBar";
+import AdminLayout from "../../layouts/AdminLayout";
 
 export default function AdminDashboardPage() {
   return (
-    <div className="ml-64 min-h-screen bg-gray-50">
-      {/* Side bar */}
-      <AdminSideBar></AdminSideBar>
-
-      <div className="min-h-screen bg-gray-50 px-5 py-6">
-        {/* Page title */}
-        <div className="mb-8">
-          <h1 className="text-3xl font-bold text-gray-900">Admin Dashboard</h1>
-
-          <p className="mt-2 text-gray-500">View restaurant analytics.</p>
-        </div>
+    <AdminLayout 
+      title="Admin Dashboard"
+      description="View analytics">
+      
+      <div>
       </div>
-    </div>
+  
+    </AdminLayout>
   );
 }
