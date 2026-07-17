5f5bc12358860a1dc8aeb591ba89cd5c322fd792
Xy <67408718+xiiyann@users.noreply.github.com>
2026-07-18 03:31:04 +0800

    Updated Admin Order Page so that it implements admin layout

--- src/pages/admin/AdminOrderPage.tsx
+++ src/pages/admin/AdminOrderPage.tsx
@@ -3,23 +3,22 @@
 import { useState } from "react";
 import StatusFilterBar from "../../components/admin/order/StatusFilterBar";
 import OrderTable from "../../components/admin/order/OrderTable";
-import AdminSideBar from "../../components/admin/AdminSideBar";
+import AdminLayout from "../../layouts/AdminLayout";
 
 export default function AdminOrdersPage() {
   const [status, setStatus] = useState("all");
 
   return (
-    <div className="ml-64 min-h-screen bg-gray-50">
-      <AdminSideBar />
+    <AdminLayout 
+      title="Admin Order Page">
 
-      <main className="bg-orange-50 px-8 py-8">
+      <main className="bg-orange-50 px-2 py-8">
         <StatusFilterBar status={status} onChange={setStatus} />
 
-        <div className="p-4">
-          <h1 className="text-2xl font-bold mb-4">Admin Order Page</h1>
-          <OrderTable />
-        </div>
+        {/* <div className="p-4"> */}
+        <OrderTable />
+        {/* </div> */}
       </main>
-    </div>
+    </AdminLayout>
   );
 }
