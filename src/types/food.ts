fba731b400b5f8b51a0585173935e617ae30655b
Xy <67408718+xiiyann@users.noreply.github.com>
2026-07-18 02:08:16 +0800

    Removed estimatedMinutes and made isAvailable required for food item type

--- src/types/food.ts
+++ src/types/food.ts
@@ -4,9 +4,8 @@ export type FoodItem = {
   description?: string;
   price: number;
   image: string;
-  estimatedMinutes?: number;
   isRecommended?: boolean; // for food recommendations
-  isAvailable?: boolean; // to indicate if the food item is available for ordering
+  isAvailable: boolean; // to indicate if the food item is available for ordering
   category: {
     name: string;
   };
