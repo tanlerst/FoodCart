12d72ef6fd88cba844085343b6364e205e6e85dc
Xy <67408718+xiiyann@users.noreply.github.com>
2026-07-20 21:05:06 +0800

    Added surprise order placed page

--- src/pages/surprise/SurpriseOrderPlacedPage.tsx
+++ src/pages/surprise/SurpriseOrderPlacedPage.tsx
@@ -1,6 +1,45 @@
-/**
- *  @file CategoryCard.tsx
- *  @author Xi Yan 
- *  @version 1.0.0
- *  @description This file is the surprise order placed page.           
- */
+import OrderPlacedActionButtons from "../../components/surprise/surpriseOrderPlaced/OrderPlacedActionButtons";
+import OrderSuccessBanner from "../../components/surprise/surpriseOrderPlaced/OrderPlacedBanner";
+
+export default function SurpriseOrderPlacedPage() {
+
+  function handleOrderAgain() {
+    // navigate to surprise order page
+  }
+
+  function handleBackMenu() {
+    // back to menu
+  }
+
+  return (
+    <main className="min-h-screen bg-orange-50 p-4">
+      <div className="relative mx-auto min-h-screen max-w-md rounded-2xl p-10 shadow-md">
+    {/* <main className="min-h-dvh bg-orange-50 sm:px-4 sm:py-6">
+      <div className="mx-auto flex min-h-dvh w-full max-w-md flex-col bg-white px-6 pb-[calc(2rem+env(safe-area-inset-bottom))] pt-[calc(1rem+env(safe-area-inset-top))] sm:min-h-0 sm:rounded-3xl sm:shadow-lg"> */}
+        {/* Title */}
+        <header className="text-center">
+          <h1 className="text-2xl font-bold text-gray-950">
+            Order Placed!
+          </h1>
+        </header>
+
+        <div className="pt-15">
+          <OrderSuccessBanner />
+
+        </div>
+
+       
+
+        <div className="pt-15">
+          <OrderPlacedActionButtons
+          onOrderAgain={handleOrderAgain}
+          onBackMenu={handleBackMenu}
+        />
+      </div>
+
+    </div>
+
+        
+    </main>
+  );
+}
