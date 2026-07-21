5a9b7edec07f19e50f2746cf70d67fb1fdf5f6ef
Xy <67408718+xiiyann@users.noreply.github.com>
2026-07-22 04:23:27 +0800

    Temp app tsx to show admin edit and new item pages

--- src/App.tsx
+++ src/App.tsx
@@ -60,7 +60,8 @@ export default function App() {
 
   return (
     <BrowserRouter>
-      <AdminEditItemPage/>
+      {/* <AdminEditItemPage/> */}
+      <AdminAddItemPage/>
     </BrowserRouter>
 
   );
