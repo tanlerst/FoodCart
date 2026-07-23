b7a011602daf7f682a454cc2343a8fe55f441e93
Xy <67408718+xiiyann@users.noreply.github.com>
2026-07-24 02:42:06 +0800

    Added filter wheel item helper skeleton

---
+++ src/helpers/wheel/filterWheelItem.ts
@@ -0,0 +1,9 @@
+import type { WheelItem } from "../../types/wheelItem";
+
+export function filterEligibleWheelItems(
+  items: WheelItem[],
+  includeDrinks: boolean,
+) {
+  return true;
+  //TODO: filter according to user drink preferences
+}
