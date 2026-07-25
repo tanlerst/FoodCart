a3788444bcb62eb5e2b539b590ececa71900995d
Xy <67408718+xiiyann@users.noreply.github.com>
2026-07-25 23:53:12 +0800

    Updated food grid so that menu page fits to mobile screen

--- src/components/menu/FoodGrid.tsx
+++ src/components/menu/FoodGrid.tsx
@@ -7,7 +7,7 @@ type FoodGridProps = {
 
 export default function FoodGrid({ foods }: FoodGridProps) {
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
+    <div className="grid grid-cols-1 gap-4">
       {foods.map((food) => (
         <FoodCard key={food.name} food={food} />
       ))}
