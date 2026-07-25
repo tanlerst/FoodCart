eaf3b3338be90d0c2eb0285d5c5219680b2fb917
Xy <67408718+xiiyann@users.noreply.github.com>
2026-07-25 23:56:44 +0800

    Updated menu page to implement user layout

--- src/pages/MenuPage.tsx
+++ src/pages/MenuPage.tsx
@@ -1,9 +1,8 @@
 import FoodGrid from "../components/menu/FoodGrid";
 import SearchBar from "../components/menu/SearchBar";
-import NavigationBar from "../components/common/NavigationBar";
 import CategoryTabs from "../components/menu/CategoryTabs";
 import { menuData } from "../helpers/menu/menuData";
-import LogoutButton from "../components/auth/LogoutButton";
+import UserLayout from "../layouts/UserLayout";
 
 export default function MenuPage() {
   const { filteredFoods, categories, search, setSearch, catTab, setCatTab } = menuData();
@@ -13,20 +12,12 @@ export default function MenuPage() {
       ? "All Categories"
       : `${catTab} Category`;
   return (
-    <div className="container mx-auto px-4 py-8 pb-30">
-      <div className="mb-6 flex items-center justify-between">
-        <h1 className="text-3xl font-bold">{pageTitle}</h1>
-
-        <LogoutButton />
-      </div>
+    <UserLayout title={pageTitle}>
       <SearchBar searchWord={search} setSearchWord={setSearch} />
 
       <CategoryTabs categories={categories} chosen_category={catTab} changeCategory={setCatTab} />
       <FoodGrid foods={filteredFoods} />
 
-      <div className="fixed bottom-0 left-0 right-0 bg-orange-50">
-        <NavigationBar />
-      </div>
-    </div>
+    </UserLayout>
   );
 }
