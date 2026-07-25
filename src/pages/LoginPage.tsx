fff003250b6cceae475ac36d3587820bfe424abe
Xy <67408718+xiiyann@users.noreply.github.com>
2026-07-26 01:26:52 +0800

    Updated login page to implement auth layout

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -4,6 +4,7 @@ import { getRoute } from "../helpers/auth/getRoute";
 import { supabase } from "../utils/supabase";
 import LoginCard from "../components/auth/LoginCard";
 import SignupCard from "../components/auth/SignupCard";
+import AuthLayout from "../layouts/AuthLayout";
 
 export default function LoginPage() {
   const navigate = useNavigate();
@@ -19,20 +20,16 @@ export default function LoginPage() {
   }
 
   return (
-    <main className="min-h-screen bg-white">
-      <div className="flex min-h-screen items-center justify-center">
-        <div className="w-full max-w-md">
-          <div className="flex justify-center items-center mb-6">
-            <h1 className="text-4xl font-bold text-gray-900">Welcome Back!</h1>
-          </div>
-
-          <p className="text-center text-gray-600 mb-6">Sign in to continue your food journey</p>
-
-          <LoginCard onLogin={login} />
-
-          <SignupCard />
+      <AuthLayout >
+        <div className="flex justify-center items-center mb-6">
+          <h1 className="text-4xl font-bold text-gray-900">Welcome Back!</h1>
         </div>
-      </div>
-    </main>
+
+        <p className="text-center text-gray-600 mb-6">Sign in to continue your food journey</p>
+
+        <LoginCard onLogin={login} />
+
+        <SignupCard />
+      </AuthLayout>
   );
 }
