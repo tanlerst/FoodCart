7c97f6af102166ffee5883d056342e7fa8daa707
Xy <67408718+xiiyann@users.noreply.github.com>
2026-07-26 12:30:56 +0800

    Removed header comment for surprise me button, changed button colour to orange-500

--- src/components/surprise/surpriseOption/SurpriseMeButton.tsx
+++ src/components/surprise/surpriseOption/SurpriseMeButton.tsx
@@ -1,11 +1,3 @@
-/**
- *  @file SurpriseMeButton.tsx
- *  @author Xi Yan
- *  @version 1.0.0
- *  @description This file is the surprise me button component for the surprise option page.
- *                It displays a button that triggers the surprise action.
- */
-
 type SurpriseMeButtonProps = {
   onSurprise: () => void;
 };
@@ -15,7 +7,7 @@ export default function SurpriseMeButton({ onSurprise }: SurpriseMeButtonProps) 
     <button
       type="button"
       onClick={onSurprise}
-      className="w-100 rounded-xl bg-orange-400 px-6 py-3 font-bold text-white"
+      className="w-100 rounded-xl bg-orange-500 px-6 py-3 font-bold text-white"
     >
       Surprise Me
     </button>
